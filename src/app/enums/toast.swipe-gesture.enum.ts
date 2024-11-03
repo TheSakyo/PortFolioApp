c1d2c7c642b8a2a8256c70ded73e69843da8c2de
TheSakyo <sacha.cardone@gmail.com>
2024-11-03 20:41:32 +0100

    Ajout d'un service de gestion des notifications toast + personnalisation de certaines propriétés en les rendant configurable avec l'aide d'énumérations

---
+++ src/app/enums/toast.swipe-gesture.enum.ts
@@ -0,0 +1,7 @@
+/**
+ * Énumération des différents geste de balayage valides pour les notifications toast
+ */
+export enum EToastSwipeGesture {
+
+    VERTICAL = 'vertical', // Geste de balayage vertical
+}
--- src/app/interfaces/global/toast.options.interface.ts
+++ src/app/interfaces/global/toast.options.interface.ts
@@ -4,16 +4,18 @@ import { ToastOptions } from '@ionic/core';
 import { EToastLayout } from "src/app/enums/toast.layout.enum";
 import { EInterfaceMode } from "src/app/enums/interface.mode.enum";
 import { EToastButtonSide } from "src/app/enums/toast.button.side.enum";
+import { EToastSwipeGesture } from "src/app/enums/toast.swipe-gesture.enum";
 
 /**
  * Interface personnalisée pour les options de notification toast,
  * qui étend les options de toast de Ionic tout en remplaçant certaines propriétés par défaut par des énumérations.
  */
-export interface IToastOptions extends Omit<ToastOptions, 'color' | 'position' | 'layout' | 'mode' | 'buttons' > {
-
-    color?: EToastColor; // Couleur optionnelle pour le toast avec l'aide d'une énumération
-    position?: EToastPosition; // Position optionnelle pour le toast avec l'aide d'une énumération
-    layout?: EToastLayout; // Layout optionnel pour le toast avec l'aide d'une énumération
-    mode?: EInterfaceMode; // Mode d'interface optionnel pour le toast avec l'aide d'une énumération
+export interface IToastOptions extends Omit<ToastOptions, 'buttons' | 'position' | 'swipeGesture' | 'layout' | 'color' | 'mode'> {
+    
     buttons?: (EToastButtonSide | string)[]; // Boutons optionnels pour le toast avec l'aide d'une autre interface ou d'une chaîne de caractères
+    position?: EToastPosition; // Position optionnelle pour le toast avec l'aide d'une énumération
+    swipeGesture?: EToastSwipeGesture // Geste de balayage optionnel pour le toast avec l'aide d'une énumération
+    layout?: EToastLayout; // Layout optionnel pour le toast avec l'aide d'une énumération
+    color?: EToastColor; // Couleur optionnelle pour le toast avec l'aide d'une énumération
+    mode?: EInterfaceMode; // Mode d'interface optionnel pour le toast avec l'aide d'une énumération
 }
