bf7c7c09741311acb93f379cfbd8830326f2b34f
Sacha Crd <sacha.cardone@gmail.com>
2025-01-10 11:46:21 +0100

    Ajout d'une interface pour la gestion des sections pour le menu

---
+++ src/app/shared/interfaces/global/section.interface.ts
@@ -0,0 +1,17 @@
+export interface ISectionInterface {
+
+    /**
+     * Nom de la section.
+     */
+    name: string,
+
+    /**
+     * Icône de la section.
+     */
+    icon: string,
+
+    /**
+     * Route de la section.
+     */
+    route: string
+}
