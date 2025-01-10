6f1a4137ecf03dd689e3c0a2b585e8078679e496
Sacha Crd <sacha.cardone@gmail.com>
2025-01-10 11:57:29 +0100

    Ajout du composant 'projects-management' pour gérer les projets réalisés

---
+++ src/app/admin/components/projects-management/projects-management.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ProjectsManagementComponent } from './projects-management.component';
+
+describe('ProjectsManagementComponent', () => {
+  let component: ProjectsManagementComponent;
+  let fixture: ComponentFixture<ProjectsManagementComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProjectsManagementComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ProjectsManagementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/admin/components/projects-management/projects-management.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {IonicModule} from "@ionic/angular";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-projects-management',
+  templateUrl: './projects-management.component.html',
+  styleUrls: ['./projects-management.component.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class ProjectsManagementComponent {
+  projects: { title: string; description: string; tags: string[] }[] = [];
+  isEditing = false;
+  projectForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    tags: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  saveProject() {
+
+    const projectValue = this.convertValues(this.projectForm.value);
+
+    if(this.projectForm.invalid || this.projects.length <= 0 || !projectValue) return;
+
+    if(this.isEditing) {
+
+      const index = this.projects.findIndex(p => p.title === this.projectForm.value.title);
+      this.projects[index] = projectValue;
+
+    } else this.projects.push(projectValue);
+
+    this.projectForm.reset();
+    this.isEditing = false;
+  }
+
+  editProject(project: any) {
+
+    this.isEditing = true;
+    this.projectForm.setValue({
+      ...project,
+      tags: project.tags.join(', '),
+    });
+  }
+
+  deleteProject(project: any) {
+    this.projects = this.projects.filter(p => p !== project);
+  }
+
+  convertValues(values: Partial<{ title: string | null, description: string | null,  tags: string | null }>) : Project | null {
+
+    if(!values.title || !values.description || !values.tags || values.tags.split(',').every(tag => tag.trim().length > 0)) return null;
+    else return {
+      title: values.title,
+      description: values.description,
+      tags: values.tags.split(',').map(tag => tag.trim()),
+    }
+  }
+}
+
+interface Project {
+  title: string;
+  description: string;
+  tags: string[];
+}
