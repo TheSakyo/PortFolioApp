3b327b8de0c499ba528a67751382a7925fb4dffc
Sacha Crd <sacha.cardone@gmail.com>
2025-01-10 11:54:14 +0100

    Modification du script de routing principal pour inclure les routes du dossier 'main' et du dossier 'admin'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,29 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-  /*************************************************************/
+  /***************************************************************/
   /*********************** ⬇️ PRINCIPAL ⬇️ ***********************/
-  /*************************************************************/
-  
+  /***************************************************************/
+
   {
-    path: '', 
-    loadComponent: () => import('./content/content.page').then(page => page.ContentPage), 
-    pathMatch: 'full',
+    path: '',
+    loadChildren: () =>
+      import('./main/main.route').then((m) => m.MAIN_ROUTES),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.route').then((m) => m.ADMIN_ROUTES),
   },
 
-  /**********************************************************/
+  /*************************************************************/
   /*********************** ⬇️ ERREUR ⬇️ ***********************/
-  /**********************************************************/
-  
+  /************************************************************/
+
   {
-    path: '**',  
-    loadComponent: () => import('./components/errors/error-404/error404.component').then(component => component.Error404Component), 
+    path: '**',
+    loadComponent: () =>
+      import('./shared/components/errors/error-404/error404.component').then(component => component.Error404Component),
     pathMatch: 'full',
-  }
+  },
 ];
