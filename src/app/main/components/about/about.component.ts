0530b241d262482b243f2f62228696f768f5b56e
Sacha Crd <sacha.cardone@gmail.com>
2025-01-14 19:06:43 +0100

    Ajoute des métadonnées pour la page 'À Propos' avec l'aide de son service

--- src/app/main/components/about/about.component.ts
+++ src/app/main/components/about/about.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { DarkLightToggleComponent } from '../../../shared/components/elements/dark-light-toggle/dark-light-toggle.component';
 import { AnimationService } from 'src/app/shared/services/animation.service';
+import { SeoService } from "../../../shared/services/seo.service";
 
 @Component({
   selector: 'sakyo-about',
@@ -12,7 +12,6 @@ import { AnimationService } from 'src/app/shared/services/animation.service';
   imports: [
     IonicModule,
     CommonModule,
-    DarkLightToggleComponent
   ]
 })
 export class AboutComponent implements OnInit {
@@ -25,8 +24,10 @@ export class AboutComponent implements OnInit {
    * Constructeur du composant 'À Propos de moi'.
    * @param elementRef - Référence à l'élément racine du composant.
    * @param animationService - Le service correspondant à la gestion des animations de l'application.
+   * @param seoService - Le service correspondant à la gestion du SEO de l'application.
    */
-  constructor(private elementRef: ElementRef, private animationService: AnimationService) {}
+  constructor(private elementRef: ElementRef, private animationService: AnimationService,
+              private seoService: SeoService) {}
 
   /********************************************************/
   /**************   ⬇️    CYCLE DE VIE    ⬇️   **************/
@@ -36,7 +37,19 @@ export class AboutComponent implements OnInit {
    * Lors de l'initialisation du composant, on initialise l'animation du scroll.
    */
   ngOnInit() {
-    this.animationService.initScroll(this.elementRef); // Initialisation de l'animation du scroll à l'aide de son service
+
+    // Initialisation de l'animation du scroll à l'aide de son service
+    this.animationService.initScroll(this.elementRef);
+
+    /*
+     * On définit les données SEO de la page.
+     */
+    this.seoService.setSeoData('À Propos - PortFolio de Sacha',
+        {
+            'description': 'Découvrez mon parcours, mes compétences et mes expériences professionnelles.',
+            'keywords': 'sacha cardone, parcours, compétences, expériences, professionnelles, portfolio'
+        }
+    );
   }
 }
 
