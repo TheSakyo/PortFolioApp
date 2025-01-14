510bcce867419ec718b9d86c63af364fe237dc46
Sacha Crd <sacha.cardone@gmail.com>
2025-01-14 19:03:47 +0100

    Ajout d'une interface pour les erreurs du formulaire

---
+++ src/app/shared/interfaces/forms/form.error.interface.ts
@@ -0,0 +1,16 @@
+export interface IFormError {
+
+    /*****************************************************/
+    /**************   ⬇️    PROPRIÉTÉS    ⬇️   **********/
+    /****************************************************/
+
+    /**
+     * Nom de l'erreur.
+     */
+    name: string,
+
+    /**
+     * Message de l'erreur.
+     */
+    message?: string
+}
