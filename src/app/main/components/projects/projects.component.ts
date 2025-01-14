c7a28de9f29ffb491d1f630962591ef9f4df3784
Sacha Crd <sacha.cardone@gmail.com>
2025-01-14 19:06:32 +0100

    Ajoute des métadonnées pour la page des projets avec l'aide de son service

--- src/app/main/components/projects/projects.component.ts
+++ src/app/main/components/projects/projects.component.ts
@@ -2,6 +2,7 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { AnimationService } from 'src/app/shared/services/animation.service';
+import { SeoService } from "../../../shared/services/seo.service";
 
 @Component({
   selector: 'sakyo-projects',
@@ -16,26 +17,40 @@ import { AnimationService } from 'src/app/shared/services/animation.service';
 })
 export class ProjectsComponent implements OnInit {
 
-  /********************************************************/
+  /***********************************************************/
   /**************   ⬇️    CONSTRUCTEUR    ⬇️   **************/
-  /********************************************************/
+  /**********************************************************/
 
   /**
    * Constructeur du composant 'Mes Projets'.
    * @param elementRef - Référence à l'élément racine du composant.
    * @param animationService - Le service correspondant à la gestion des animations de l'application.
+   * @param seoService - Le service correspondant à la gestion du SEO de l'application.
    */
-  constructor(private elementRef: ElementRef, private animationService: AnimationService) {}
+  constructor(private elementRef: ElementRef, private animationService: AnimationService,
+              private seoService: SeoService) {}
 
-  /********************************************************/
+  /***********************************************************/
   /**************   ⬇️    CYCLE DE VIE    ⬇️   **************/
-  /********************************************************/
+  /**********************************************************/
 
   /**
    * Lors de l'initialisation du composant, on initialise l'animation du scroll.
    */
   ngOnInit() {
-    this.animationService.initScroll(this.elementRef); // Initialisation de l'animation du scroll à l'aide de son service
+
+    // Initialisation de l'animation du scroll à l'aide de son service
+    this.animationService.initScroll(this.elementRef);
+
+    /*
+     * On définit les données SEO de la page.
+     */
+    this.seoService.setSeoData('Projets - PortFolio de Sacha',
+      {
+        'description': 'Explorez mes projets de développement et mes réalisations professionnelles.',
+        'keywords': 'projets, développement, portfolio, réalisations, professionnels, sacha cardone'
+      }
+    );
   }
 }
 
