1d589b4c8da249579662862c996f5b8d1b6382b7
Sacha Crd <sacha.cardone@gmail.com>
2025-01-17 18:00:07 +0100

    Modification des méthodes 'isPromise' et 'isObservable' pour vérifier également dans chaque valeur d'un tableau

--- src/app/shared/utils/asynchrone-check.utils.ts
+++ src/app/shared/utils/asynchrone-check.utils.ts
@@ -2,6 +2,31 @@ import { Observable } from "rxjs";
 
 export abstract class AsynchroneCheckUtils {
 
+    /************************************************************************/
+    /**************   ⬇️    PROPRIÉTÉS AVEC FONCTIONS    ⬇️   **************/
+    /************************************************************************/
+
+
+    /**
+     * Vérifie si une valeur est une promesse.
+     * @param value - La valeur qui doit être vérifiée.
+     * @returns - Le résultat de la vérification.
+     */
+    private static isInstanceOfPromise: (value: any) => boolean = (value: any) => value instanceof Promise;
+
+    /**
+     * Vérifie si une valeur est un observable.
+     * @param value - La valeur qui doit être vérifiée.
+     * @returns - Le résultat de la vérification.
+     */
+    private static isInstanceOfObservable: (value: any) => boolean = (value: any) => value instanceof Observable;
+
+
+    /*******************************************************/
+    /**************   ⬇️    MÉTHODES    ⬇️   **************/
+    /******************************************************/
+
+
     /**
      * Vérifie si une valeur est une promesse.
      * @param value - La valeur qui doit être vérifiée.
@@ -11,8 +36,15 @@ export abstract class AsynchroneCheckUtils {
 
         if(!value) return false;
 
-        const isInstanceOfPromise = value instanceof Promise;
-        return isInstanceOfPromise || typeof value.then === 'function';
+        if(Array.isArray(value)) {
+
+            for(const item of value) {
+                if(!this.isInstanceOfPromise(item) && typeof item.then !== 'function') return false;
+            }
+
+            return true;
+
+        } else return this.isInstanceOfPromise(value) || typeof value.then === 'function';
     }
 
     /**
@@ -24,7 +56,14 @@ export abstract class AsynchroneCheckUtils {
 
         if(!value) return false;
 
-        const isInstanceOfObservable = value instanceof Observable;
-        return isInstanceOfObservable || typeof value.subscribe === 'function';
+        if(Array.isArray(value)) {
+
+            for(const item of value) {
+                if(!this.isInstanceOfObservable(item) && typeof item.subscribe !== 'function') return false;
+            }
+
+            return true;
+
+        } else return this.isInstanceOfObservable(value) || typeof value.subscribe === 'function';
     }
 }
