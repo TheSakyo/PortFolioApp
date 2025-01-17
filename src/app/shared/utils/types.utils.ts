4a8befcfab49a59d29349baa7473229ec138800c
Sacha Crd <sacha.cardone@gmail.com>
2025-01-17 18:02:32 +0100

    Modification des types personnalisée et ajout d'un nouveau type 'FormControlValue' pour représenter un contrôle d'un formulaire et son type d'erreur

--- src/app/shared/utils/types.utils.ts
+++ src/app/shared/utils/types.utils.ts
@@ -1,21 +1,30 @@
-import { AsyncValidatorFn, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { AsyncValidatorFn, FormControlOptions, ValidatorFn } from "@angular/forms";
+import { IFormError } from "@portfolio/shared/interfaces/forms/form.error.interface";
 
 /**
  * Type pour les validations des formulaires.
  * @remarks - On utilise soit un {@link ValidatorFn} ou un tableau {@link ValidatorFn[]}.
- * @type {ValidatorFn | ValidatorFn[]}
  */
-export type ValidatorsFnType = ValidatorFn | ValidatorFn[];
+export type ValidatorsFn = ValidatorFn | ValidatorFn[];
 
 /**
  * Type pour les validations des formulaires
  * @remarks - On utilise soit un {@link AsyncValidatorFn} ou un tableau de {@link AsyncValidatorFn[]}.
- * @type {ValidatorFn[] | AsyncValidatorFn[]}
  */
-export type AsyncValidatorsFnType = AsyncValidatorFn | AsyncValidatorFn[]
+export type AsyncValidatorsFn = AsyncValidatorFn | AsyncValidatorFn[]
 
 /**
  * Type pour les erreurs de validation avec un message personnalisé.
- * @type {ValidationErrorsWithMessageType}
+ * @see  IFormError
  */
-export type ValidationErrorsWithMessageType = { [key: string]: { type: keyof ValidationErrors, message: string }[] };
+export type ValidationErrorsWithMessage = { [key: string]: IFormError[] };
+
+/**
+ * Type pour les valeurs des formulaires.
+ */
+export type FormControlValue = { [key: string]: { value: any, options?: FormControlOptions } };
+
+/**
+ * Type pour les erreurs de contrôle des formulaires (utilisé lors de la vérification des erreurs).
+ */
+export type FormControlError = { targetName: string, type: string | 'ANY' }
