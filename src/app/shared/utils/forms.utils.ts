c507aff317b8cdd038f5bb901576bce3c9a9127c
Sacha Crd <sacha.cardone@gmail.com>
2025-01-17 18:01:07 +0100

    Modification des méthodes 'hasError' et 'getErrorMessages' en ajoutant le nouveau type 'FormControlError' permettant une plus grande personnalisation

--- src/app/shared/utils/forms.utils.ts
+++ src/app/shared/utils/forms.utils.ts
@@ -1,4 +1,7 @@
-import { FormService } from "../services/form.service";
+import { AbstractControl } from "@angular/forms";
+import { IFormError } from "@portfolio/shared/interfaces/forms/form.error.interface";
+import { FormService } from "@portfolio/shared/services/common/form.service";
+import { FormControlError } from "@portfolio/shared/utils/types.utils";
 
 export abstract class FormsUtils {
 
@@ -27,22 +30,46 @@ export abstract class FormsUtils {
     /**
      * Vérifie si le champ en question du formulaire a une erreur.
      * @param formName - Le nom du formulaire à vérifier.
-     * @param controlName - Le nom du champ à vérifier.
-     * @param error - Le type d'erreur à vérifier.
+     * @param controlError - Les informations sur le champ et le nom de l'erreur qui doit être vérifiée.
      */
-    public static hasError(formName: string, controlName: string, error: string): boolean {
+    public static hasError(formName: string, controlError: FormControlError): boolean {
+
         this.formService.selectForm = formName;
-        return this.formService.checkControlErrors(controlName, error);
+
+        switch(controlError.type) {
+            case 'ANY':
+                return this.formService.getControlErrors(controlError.targetName).length > 0;
+            default:
+                return this.formService.checkControlErrors(controlError.targetName, controlError.type);
+        }
     }
 
     /**
      * Récupère le message d'erreur associé à un champ du formulaire.
      * @param formName - Le nom du formulaire à vérifier.
-     * @param controlName - Le nom du champ à vérifier.
-     * @param error - Le type d'erreur à vérifier.
+     * @param controlError - Les informations sur le champ et le nom de l'erreur qui doit être vérifiée.
+     * @param onlyValidError - Indique si on doit récupérer seulement les erreurs valides.
      */
-    public static getErrorMessage(formName: string, controlName: string, error: string): string | undefined {
+    public static getErrorMessage(formName: string, controlError: FormControlError, onlyValidError: boolean = false): string | undefined {
+
+        const formControl: AbstractControl | null = this.formService.formControl(formName, controlError.targetName);
+
+        let errors: IFormError[] = this.formService.getControlErrors(controlError.targetName);
+        if(onlyValidError) errors = errors.filter(error => formControl && error.validator(formControl));
+
         this.formService.selectForm = formName;
-        return this.formService.getControlError(controlName, error)?.message;
+
+        switch(controlError.type) {
+
+            case 'ANY':
+                if(errors) return errors.length > 0 ? errors[0].message : undefined;
+                return undefined;
+            default:
+                if(errors) {
+                    if(onlyValidError) errors = errors.filter(error => error.type === controlError.type);
+                    return errors.length > 0 ? errors.map(error => error.message).join(' - ') : undefined;
+                }
+                return undefined;
+        }
     }
 }
