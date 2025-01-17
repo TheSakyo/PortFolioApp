bc661f4549daca1a23ce10f43a41520bdfbfc575
Sacha Crd <sacha.cardone@gmail.com>
2025-01-17 18:20:54 +0100

    Changement de l'exception personnalisée par 'NotValidError'

--- src/app/shared/exceptions/NotValidError.ts
+++ src/app/shared/exceptions/NotValidError.ts
@@ -1,14 +1,14 @@
-export class NotFoundError extends Error {
+export class NotValidError extends Error {
 
     /**
-     * Constructeur de l'erreur {@link NotFoundError}.
+     * Constructeur de l'erreur {@link NotValidError}.
      * @param message - Le message d'erreur.
      */
     constructor(message: string) {
 
         super(message); // Appel du constructeur de la classe mère
 
-        this.name = 'NotFoundError'; // Définition du nom de l'erreur
+        this.name = 'NotValidError'; // Définition du nom de l'erreur
         this.message = message; // Définition du message d'erreur
     }
 }
