3ce3febdc5b5dee3b379776394336f280fe7823e
Sacha Crd <sacha.cardone@gmail.com>
2025-01-17 18:19:39 +0100

    Ajout d'une nouvelle propriété validator servant à pouvoir définir le validateur en question pour activer l'erreur du contrôle dans un formulaire

--- src/app/shared/interfaces/forms/form.error.interface.ts
+++ src/app/shared/interfaces/forms/form.error.interface.ts
@@ -1,3 +1,5 @@
+import { AsyncValidatorFn, ValidatorFn } from "@angular/forms";
+
 export interface IFormError {
 
     /*****************************************************/
@@ -5,12 +7,42 @@ export interface IFormError {
     /****************************************************/
 
     /**
-     * Nom de l'erreur.
+     * Type de l'erreur.
      */
-    name: string,
+    type: string,
 
     /**
      * Message de l'erreur.
      */
-    message?: string
+    message: string
+
+    /**
+     * Fonction de validation.
+     * <br/><br/><br/><br/>
+     * @example Voici quelques exemples avec l'utilisation de la propriété '**validator**' :
+     *
+     *     // ⬇️ Exemple avec 'Validators' ⬇️//
+     *     {
+     *      type: 'required', // Type de validation
+     *      message: 'Le champ est obligatoire.', // Message d'erreur
+     *      validator: Validators.required // Validation requise
+     *     }
+     *
+     *     // ⬇️ Exemple avec une validation personnalisée ⬇️//
+     *     {
+     *      type: 'test', // Type de validation
+     *      message: 'Le champ ne doit pas contenir le mot "test".', // Message d'erreur
+     *      validator: validateTest() // Validation personnalisée
+     *     }
+     *
+     *     ...
+     *
+     *     // Fonction de validation personnalisée 'validateTest'
+     *     function validateTest(): ValidatorFn {
+     *      return (control: AbstractControl): ValidationErrors | null => {
+     *          if (control.value === 'test') return { test: { message: 'Le champ ne doit pas contenir le mot "test".' }
+     *          return null;
+     *      };
+     */
+    validator: ValidatorFn | AsyncValidatorFn
 }
